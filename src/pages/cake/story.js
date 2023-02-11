ee6043166fc3fc88091a6b2902ba22a86b295cfb
JstVex <heinhtetlulu14@gmail.com>
2023-02-11 21:43:08 +0630

    Images for cake story

--- src/pages/cake/story.js
+++ src/pages/cake/story.js
@@ -172,7 +172,7 @@ export default function Story() {
             <section className={styles.second}>
                 <Image
                     className={styles.story_start}
-                    src="https://images.unsplash.com/photo-1588195538326-c5b1e9f80a1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80"
+                    src="https://dsm01pap007files.storage.live.com/y4m2-wSto1axMksIKjly5qOI_wqfyTxr1cjyY9STzpCAkT0bx4Urua-5yl2b2XFDSiNtXCBB05xt07tKO6wWn11CaB-JMioqbq6w2Ys-Lj9Wym2ciF3_9GPzqM5hzgKvNxJzqCO3Ew7ChIFCJhpaLWaRizpb73dnFkGm3h9rn0ZGJW2W_zLzakkMz4hmwl9lM76?width=1522&height=913&cropmode=none"
                     alt="Cake"
                     width={1000}
                     height={1000}
@@ -181,7 +181,7 @@ export default function Story() {
                 <div className={styles.story_wrap}>
                     <Image
                         className={styles.story_image}
-                        src="https://dsm01pap007files.storage.live.com/y4mqJ_QlXxFxF8t887pMQsm7tl6udD3PX73mB3aVQtqbyai2TgK-Y4ySv_s65M9hUWT01rxpkVw9Hnk-rO3IARtYY-gSR5c7qtRggr8Fefp8a2YN6iK_gRdJk1StwXR2mAkvIp2GpkKRcXK2ehUV6B_jHZgjGtdvXok3v1-G2I5OW-3TuuYZxfefIdoOC17TDFT?width=2200&height=1500&cropmode=none"
+                        src="https://dsm01pap007files.storage.live.com/y4mA3MecwRXCffgkwj_Jc5S2Euz-28vvqLZbNzAsdyOgw3XRKYA2D-pIs8rzZX5ZDW3q6zV1U2ErLKHOReJq_oYsqlpq7VxY_n2Yq3uhMvOZ-u3s90eMVI9CGlwns17aeS4CX2Yr3GBcoqoKJDRijCEaS-seGN0mKyiMEa1VDR4KhkJqGtuhpanTWmir_2hxPW4?width=2500&height=1500&cropmode=none"
                         alt="Cake"
                         width={1000}
                         height={1000}
@@ -195,70 +195,70 @@ export default function Story() {
                 <div className={styles.story_wrap}>
                     <Image
                         className={styles.story_image}
-                        src="https://dsm01pap007files.storage.live.com/y4mqJ_QlXxFxF8t887pMQsm7tl6udD3PX73mB3aVQtqbyai2TgK-Y4ySv_s65M9hUWT01rxpkVw9Hnk-rO3IARtYY-gSR5c7qtRggr8Fefp8a2YN6iK_gRdJk1StwXR2mAkvIp2GpkKRcXK2ehUV6B_jHZgjGtdvXok3v1-G2I5OW-3TuuYZxfefIdoOC17TDFT?width=2200&height=1500&cropmode=none"
+                        src="https://dsm01pap007files.storage.live.com/y4mWEGeoR2H2au75LEjsWRm-2hUaj05Y-JxUNpSD6nFvuTWV0jgKbubfw58VEho32iwEWRGfJ_f6ecvTxb0FCp8hZQngzqcVQmgkFwl2Clhd48mUX3gnpPB59Y-1SKa2NvEixc_3YU_7327BP1f_b8CaJmKmfh3YTaCvJ-EheEYQ74BPVhxqxQTDFu8FCpq_hwE?width=2500&height=1500&cropmode=none"
                         alt="Cake"
                         width={1000}
                         height={1000}
                         priority
                     />
                     <div className={styles.story_text}>
-                        ~ ur chef bf thinking he is capable of baking cakes ~
+                        ~ struggling to find the best n suitable recipe ~
                     </div>
                 </div>
 
                 <div className={styles.story_wrap}>
                     <Image
                         className={styles.story_image}
-                        src="https://dsm01pap007files.storage.live.com/y4mqJ_QlXxFxF8t887pMQsm7tl6udD3PX73mB3aVQtqbyai2TgK-Y4ySv_s65M9hUWT01rxpkVw9Hnk-rO3IARtYY-gSR5c7qtRggr8Fefp8a2YN6iK_gRdJk1StwXR2mAkvIp2GpkKRcXK2ehUV6B_jHZgjGtdvXok3v1-G2I5OW-3TuuYZxfefIdoOC17TDFT?width=2200&height=1500&cropmode=none"
+                        src="https://dsm01pap007files.storage.live.com/y4mLgRAwFiuBpz60vr35gFstMTg9148HBsWwf0CjUAA1CXEFrniPln8hxlzjrZf35gw5b4xB2-2nvsz8b_w4pYWrx_hCSPqizSDWwzTqABGh4yx3MRlQ8G-iLnlbLhtwUOfY1i2AOBOfMiRusTt9U0U8i3hSiEQV6-8-RIS22fXl79XiEuTSrbzFa5oeCjZdfOM?width=2500&height=1500&cropmode=none"
                         alt="Cake"
                         width={1000}
                         height={1000}
                         priority
                     />
                     <div className={styles.story_text}>
-                        ~ ur chef bf thinking he is capable of baking cakes ~
+                        ~ going with cheesecake cuz thats all im able to:&#40; ~
                     </div>
                 </div>
 
                 <div className={styles.story_wrap}>
                     <Image
                         className={styles.story_image}
-                        src="https://dsm01pap007files.storage.live.com/y4mqJ_QlXxFxF8t887pMQsm7tl6udD3PX73mB3aVQtqbyai2TgK-Y4ySv_s65M9hUWT01rxpkVw9Hnk-rO3IARtYY-gSR5c7qtRggr8Fefp8a2YN6iK_gRdJk1StwXR2mAkvIp2GpkKRcXK2ehUV6B_jHZgjGtdvXok3v1-G2I5OW-3TuuYZxfefIdoOC17TDFT?width=2200&height=1500&cropmode=none"
+                        src="https://dsm01pap007files.storage.live.com/y4mfQUlWjq-qubj9-hWs1LD98tgWhGDU7Cp44nchDNk7uAfLuncU--W_aolkh1bSg-ER24ahb4bjmjeod6OOGYjy41MUCju71oHfxlITLc6KGOFkmArS9wlntjoAxGeVAJSM1EVurX1v5dmryJ7QfBzUpEYu6JQlz10aTNtzQskKXkBZ0QkpLNmMqmad-NRmX8W?width=2500&height=1500&cropmode=none"
                         alt="Cake"
                         width={1000}
                         height={1000}
                         priority
                     />
                     <div className={styles.story_text}>
-                        ~ ur chef bf thinking he is capable of baking cakes ~
+                        ~ whisking cheesecake &#62;&#62;&#62;&#62;&#62;&#62;&#62; hitting gym ~
                     </div>
                 </div>
 
                 <div className={styles.story_wrap}>
                     <Image
                         className={styles.story_image}
-                        src="https://dsm01pap007files.storage.live.com/y4mqJ_QlXxFxF8t887pMQsm7tl6udD3PX73mB3aVQtqbyai2TgK-Y4ySv_s65M9hUWT01rxpkVw9Hnk-rO3IARtYY-gSR5c7qtRggr8Fefp8a2YN6iK_gRdJk1StwXR2mAkvIp2GpkKRcXK2ehUV6B_jHZgjGtdvXok3v1-G2I5OW-3TuuYZxfefIdoOC17TDFT?width=2200&height=1500&cropmode=none"
+                        src="https://dsm01pap007files.storage.live.com/y4mtnkIBvCJ_PeGIbH1J332fUNBohkKseeQUAaV9maz0Wzc5GdXC01pdma8QlZ4WH8K-GktrajKJEPL6iRmAtUJFJ8EXRAydHHelTT6Tit4dy6Ts45kqT5ytrtgozLFsN64Soz5ZT0g0duviO9XYrz6Mpb7qn-YR4mKv6eTUK8Nx4wg4mqokPgInPxqCuuH1xbg?width=2500&height=1500&cropmode=none"
                         alt="Cake"
                         width={1000}
                         height={1000}
                         priority
                     />
                     <div className={styles.story_text}>
-                        ~ ur chef bf thinking he is capable of baking cakes ~
+                        ~ most nervous-able time to see how it turns out ~
                     </div>
                 </div>
 
                 <div className={styles.story_wrap}>
                     <Image
                         className={styles.story_image}
-                        src="https://dsm01pap007files.storage.live.com/y4mqJ_QlXxFxF8t887pMQsm7tl6udD3PX73mB3aVQtqbyai2TgK-Y4ySv_s65M9hUWT01rxpkVw9Hnk-rO3IARtYY-gSR5c7qtRggr8Fefp8a2YN6iK_gRdJk1StwXR2mAkvIp2GpkKRcXK2ehUV6B_jHZgjGtdvXok3v1-G2I5OW-3TuuYZxfefIdoOC17TDFT?width=2200&height=1500&cropmode=none"
+                        src="https://dsm01pap007files.storage.live.com/y4mUL6XFa1kJvsGesuROz54r35qRNa4dw8VCA_AtiU1634DMaCXA0-1y1OhShyii_-GCRkh8x6vrhgXK1gb2tKtPQVgDlCC6jyXUnVOFl0Z_lAyVi6agTfm5ed-I9EfbEEwSWKlF7qpcKz3Q7C7l4Qg8heem0adbrPBD_NytpEum_HxlkubJDvwebWsG7gk4hON?width=2500&height=1500&cropmode=none"
                         alt="Cake"
                         width={1000}
                         height={1000}
                         priority
                     />
                     <div className={styles.story_text}>
-                        ~ ur chef bf thinking he is capable of baking cakes ~
+                        ~ cheesecake for my precious eapm lyy &#62;&#60; its simple but hope u like it ^^ ~
                     </div>
                 </div>
 
